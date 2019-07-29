e8591936f0c8bd002f937735d92cd4aff9b730a1
mamiours <36891934+mamiours@users.noreply.github.com>
2019-07-29 15:04:36 +0200

    chapter 7
    done
    - Twig syntax
    - Twig template

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -3,10 +3,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,7 +21,16 @@ class ArticleController
      * @Route("/news/{slug}")
      */
     public function show($slug){
-        return new Response(sprintf('a new article : "%s"',$slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 
     //sprintf : concatenation de la chaîne de S
