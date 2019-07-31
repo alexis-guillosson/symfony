b88c2a5bb4dd42a05109ff8eaca1805b77e8d53a
mamiours <36891934+mamiours@users.noreply.github.com>
2019-07-31 15:09:20 +0200

    Section 2 : chapter 12 & 13
    - constructor for controller
    - bundle
    - modif monolog.yaml adding channel markdown

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -13,6 +13,16 @@ use Twig\Environment;
 class ArticleController extends AbstractController
 {
     /**
+     * Currently unused: just showing a controller with a constructor!
+     */
+    private $isDebug;
+
+    public function __construct(bool $isDebug)
+    {
+        $this->isDebug = $isDebug;
+    }
+
+    /**
      * @Route("/", name="app_homepage")
      */
     public function homepage()
